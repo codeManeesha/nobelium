f3ddcac963ae0647dd32d6447e5d53f083cb4612
codeManeesha <maneeshasingh19jun@gmail.com>
2023-03-11 15:30:17 +0530

    React-Assingment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Fragment } from 'react';
-import Card from './Components/Card';
+import Form from './Components/Form';
+import Count from './Components/Count';
+import Change from './Components/Change';
 
+ 
 
 function App() {
   return (
-    <div className="App">
-    
-    
-<Card heading ="Graphic Designer" name="Amit" content="Highly creative and multitalented Graphic Designer with extensive experience in multimedia, marketing, and print design"heading1 ="Singer" name1="Ruhi" content1="perform a variety of music for recordings and live audiences. They audition for positions in choruses, orchestras, bands, plays"/>
-   
-    </div>
+    <>
+    <Form/>
+    <Count/>
+ <Change/>
+ 
+</>
   );
 }
 
