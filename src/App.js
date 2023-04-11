d27813599e02732f931f475c89ec3b5b8c2935ac
codeManeesha <maneeshasingh19jun@gmail.com>
2023-04-11 21:51:21 +0530

    masterclass_assingment2(A)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const[email,setEmail] =useState()
+  const[userName,setUserName] =useState()
+ 
+ const handleEmail=(e)=>{
+  setEmail(e.target.value)
+ }
+ 
+ const handleUserName=(e)=>{
+  setUserName(e.target.value)
+ }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="body">
+     <div className="content">
+      <div className="content1">
+      <form>
+        <h1>Form</h1>
+          <input id='em' type="text" placeholder="email" value={email} onChange={handleEmail}></input><br></br><br></br>
+          <input type="text" placeholder="username" value={userName} onChange={handleUserName}></input><br></br><br></br>
+         
+          <button type ="submit">Add</button>
+        </form>
+       </div>
+        <div className="content2">
+      
+          <h1>update contents</h1>
+        
+          <input placeholder='email'value={email}></input><br></br><br></br>
+          <input placeholder='username'value={userName}></input><br></br><br></br>
+          
+        </div>
+        </div>
+        <div className="table">
+          <table>
+          <tr>
+            
+            <th>email</th>
+            <th>username</th>
+            
+            
+          </tr>
+          <tr>
+        <td>{email}</td>
+        <td>{userName}</td>
+          </tr>
+         
+          </table>
+        </div>
+      </div>
+      
+     
+      
+  
+
   );
 }
 
