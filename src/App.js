22bf795abbc00670fa18899255dff2cfaadf47f7
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-02 20:24:08 +0530

    bonusProject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [val, setVal] = useState("");
+  const [data, setData] = useState();
+  const pack = [
+    "Express",
+    "Async.js",
+    "Grunt",
+    "PM2",
+    "Socket.IO",
+    "Commander.js",
+  ];
+  function handleSubmit() {
+
+    localStorage.setItem("content", JSON.stringify([data]));
+    setData("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        list="pack"
+        placeholder="search"
+        onChange={(e) => setVal(e.target.value)}
+      />
+      <datalist id="pack">
+        {pack.map((op) => (
+          <option>{op}</option>
+        ))}
+      </datalist>
+      <div>
+        <p>{val}</p>
+        <input id="input" placeholder='discription'value={data}onChange={(e) => setData(e.target.value)} />
+      </div>
+      <button id="button" onClick={handleSubmit}>
+        submit
+      </button>
+     
     </div>
   );
 }
