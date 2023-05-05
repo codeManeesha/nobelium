b66e6c5e329f71b7fdc36ba726621478cf0f6940
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-05 17:29:58 +0530

    converter

--- src/App.js
+++ src/App.js
@@ -1,114 +1,38 @@
 import React, { useState } from "react";
-import "./Calendar.css";
 
-const Calendar = () => {
-  const [date, setDate] = useState(new Date());
+function App() {
+  const [pixelValue, setPixelValue] = useState("");
+  const [remValue, setRemValue] = useState("");
 
-  const daysInMonth = (month, year) => {
-    return new Date(year, month + 1, 0).getDate();
+  const convertPixelToRem = (pixelValue) => {
+    const remValue = pixelValue / 16;
+    return remValue.toFixed(2);
   };
 
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-  const daysInPrevMonth = daysInMonth(
-    firstDayOfMonth.getMonth() - 1,
-    firstDayOfMonth.getFullYear()
-  );
-  const daysInCurrMonth = daysInMonth(
-    firstDayOfMonth.getMonth(),
-    firstDayOfMonth.getFullYear()
-  );
-
-  const days = [];
-
-  const handleDateClick = day => {
-    setDate(new Date(date.getFullYear(), date.getMonth(), day));
+  const handlePixelValueChange = (event) => {
+    const value = event.target.value;
+    setPixelValue(value);
+    setRemValue(convertPixelToRem(value));
   };
-
-  for (let i = firstDayOfMonth.getDay(); i > 0; i--) {
-    const day = daysInPrevMonth - i + 1;
-    days.push(
-      <div
-        key={`prev-${day}`}
-        className="day prev-month"
-        onClick={() => handleDateClick(day)}
-      >
-        {day}
-      </div>
-    );
-  }
-
-  for (let i = 1; i <= daysInCurrMonth; i++) {
-    const isCurrMonth = i === date.getDate();
-    days.push(
-      <div
-        key={`curr-${i}`}
-        className={`day curr-month ${isCurrMonth ? "selected" : ""}`}
-        onClick={() => handleDateClick(i)}
-      >
-        {i}
-      </div>
-    );
-  }
-
-  const daysInNextMonth = 7 - (days.length % 7);
-  for (let i = 1; i <= daysInNextMonth; i++) {
-    days.push(
-      <div
-        key={`next-${i}`}
-        className="day next-month"
-        onClick={() => handleDateClick(i)}
-      >
-        {i}
-      </div>
-    );
-  }
-
-  const headerText = `${monthNames[firstDayOfMonth.getMonth()]} ${firstDayOfMonth.getFullYear()}`;
 
   return (
-    <div className="calendar">
-      <div className="header">
-        <div
-          className="prev-month"
-          onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1))}
-        >
-          &lt;
-        </div>
-        <div className="month">{headerText}</div>
-        <div
-          className="next-month"
-          onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1))}
-        >
-          &gt;
-        </div>
+    <div>
+      <h2>Pixel to Rem Converter</h2>
+      <div>
+        <label htmlFor="pixel-value">Pixel Value: </label>
+        <input
+          type="number"
+          id="pixel-value"
+          value={pixelValue}
+          onChange={handlePixelValueChange}
+        />
       </div>
-      <div className="weekdays">
-        <div>Sun</div>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
-        <div>Sat</div>
+      <div>
+        <label htmlFor="rem-value">Rem Value: </label>
+        <input type="text" id="rem-value" value={remValue} readOnly />
       </div>
-      <div className="days">{days}</div>
     </div>
   );
-};
+}
 
-export default Calendar;
+export default App;
